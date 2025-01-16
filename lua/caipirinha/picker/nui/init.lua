61857580fb6162ec210fefeff083acbacb5dec83
João Inez <joaoinez.info@gmail.com>
2025-01-16 06:09:14 +0100

    fix: move setting highlights from plugin/ to nui/init.lua to avoid flicker on start

--- lua/caipirinha/picker/nui/init.lua
+++ lua/caipirinha/picker/nui/init.lua
@@ -5,6 +5,17 @@ local state = require 'caipirinha.picker.nui.state'
 local M = {}
 
 function M.pick(callback, filter)
+  local highlights = {
+    CaipirinhaButton = { default = true, link = 'CursorLine' },
+    CaipirinhaButtonActive = { default = true, link = 'Visual' },
+    CaipirinhaKeybind = { default = true, link = '@punctuation.special' },
+  }
+
+  for hl_name, hl_config in pairs(highlights) do
+    vim.api.nvim_set_hl(0, hl_name, hl_config)
+    vim.api.nvim_command 'redraw'
+  end
+
   state.callback = callback
   state.colors = colorscheme.get_installed_colorschemes(filter)
   state.filtered_colors = { unpack(state.colors) }
--- plugin/caipirinha.lua
+++ plugin/caipirinha.lua
@@ -7,14 +7,3 @@ end
 
 if vim.g.loaded_caipirinha == 1 then return end
 vim.g.loaded_caipirinha = 1
-
-local highlights = {
-  CaipirinhaButton = { default = true, link = 'CursorLine' },
-  CaipirinhaButtonActive = { default = true, link = 'Visual' },
-  CaipirinhaKeybind = { default = true, link = '@punctuation.special' },
-}
-
-for hl_name, hl_config in pairs(highlights) do
-  vim.api.nvim_set_hl(0, hl_name, hl_config)
-  vim.api.nvim_command 'redraw'
-end
